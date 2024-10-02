fd84059f3e3e6752402c9e150c7b991f244c0b1e
justin-schwinn-0 <justinsch1029@gmail.com>
2024-10-01 19:23:07 -0500

    setting up and MAP

--- MAP_Alg.cpp
+++ MAP_Alg.cpp
@@ -1,5 +1,6 @@
 #include "MAP_Alg.h"
 
+#include <thread>
 
 MAP_Alg::MAP_Alg(Node& n,int minActive, int maxActive,int maxNum):
     rNode(n),
@@ -15,7 +16,29 @@ MAP_Alg::~MAP_Alg()
 
 void MAP_Alg::handleMsg(std::string str)
 {
+    Utils::Log(str);
+}
 
+void MAP_Alg::becomeActive()
+{
+    if(!mActive)
+    {
+        std::thread activeThrd(active,this);
+        activeThrd.detach();
+    }
+
+}
+
+void MAP_Alg::active()
+{
+    rNode.flood("hello");
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    rNode.flood("hello");
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    rNode.flood("hello");
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    rNode.flood("hello");
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
 }
 
 void MAP_Alg::init()
