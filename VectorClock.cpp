5c9f018886be7301a74eaae4fb48d429510a2936
justin-schwinn-0 <justinsch1029@gmail.com>
2024-10-12 20:54:35 -0500

    finish up termination protocol and file writing

--- VectorClock.cpp
+++ VectorClock.cpp
@@ -49,9 +49,11 @@ void VectorClock::update(int sender, std::string str)
     auto splits = Utils::split(str,VC_DELIM);
 
     //Utils::log("m",to_string());
-    for(int i = 0; i < splits.size() ; i++)
+    for(int i = 0 ; i < splits.size();i++)
     {
-        mClockMap[i] = Utils::strToInt(splits[i]);
+        int val = Utils::strToInt(splits[i]) + (uid == sender);
+
+        mClockMap[uid] = std::max(val, mClockMap[uid]);
     }
     //Utils::log("o",str);
 }
