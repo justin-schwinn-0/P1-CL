5320384506b4e16ee4a1600469a400c73ada6a74
justin-schwinn-0 <justinsch1029@gmail.com>
2024-10-13 18:19:29 -0500

    finish up termination protocol and file writing

--- Node.cpp
+++ Node.cpp
@@ -168,7 +168,7 @@ void Node::releaseMessagesThread()
 {
     while(!finishedAlg )
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         releaseMessages();
     }
 }
