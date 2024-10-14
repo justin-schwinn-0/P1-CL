8efc56ec9d2c0a5666ffec337b09c77c6577429a
justin-schwinn-0 <justinsch1029@gmail.com>
2024-10-13 20:51:39 -0500

    finish up termination protocol and file writing

--- Snapshotter.cpp
+++ Snapshotter.cpp
@@ -234,7 +234,7 @@ bool Snapshotter::anyRecording()
 
 void Snapshotter::createTree()
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::this_thread::sleep_for(std::chrono::milliseconds(6000));
     rNode.flood(getCtrlStr(PARENT));
     mConvergesRemaining = rNode.getNeighborsSize();
 }
