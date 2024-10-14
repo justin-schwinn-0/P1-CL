c3c1f86d8e5c8c8dfe6487a4247a6f61ac7978bd
justin-schwinn-0 <justinsch1029@gmail.com>
2024-10-13 20:23:38 -0500

    finish up termination protocol and file writing

--- Snapshotter.cpp
+++ Snapshotter.cpp
@@ -165,7 +165,7 @@ void Snapshotter::convergeForChild()
         if(rNode.getUid() == 0)
         {
             initSnapshotProtocol();
-            //mMap.init();
+            mMap.init();
         }
     }
 }
