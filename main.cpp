cca916bebd4d15a273e429753ac0b75d4fc54fb0
justin-schwinn-0 <justinsch1029@gmail.com>
2024-10-13 21:48:14 -0500

    finish up termination protocol and file writing

--- main.cpp
+++ main.cpp
@@ -183,7 +183,7 @@ void runAlg(NodeInfo& ni)
         s.init();
     }
 
-    std::thread releaseThrd(&Node::releaseMessagesThread, &ni.n,200);
+    std::thread releaseThrd(&Node::releaseMessagesThread, &ni.n,250);
     releaseThrd.detach();
 
     ni.n.listenToNeighbors();
